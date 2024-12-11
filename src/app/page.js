e4192cd198a5c9a29d8065d098138dc0fc99d62b
NASAA0333 <nasanbuyan0333@gmail.com>
2024-12-11 18:16:28 +0800

    first upgrade

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,23 @@
+"use cilent";
+
+import "./style.css";
 export default function Home() {
   return (
-    <div
-      style={{
-        display: "flex",
-        backgroundColor: "red",
-        justifyContent: "center",
-        width: "100px",
-        height: "50px",
-      }}
-    >
-      sancir
+    <div>
+      <div className="w-full h-screen flex  justify-center items-center relative">
+        <div className="w-[50%] bg-[#F3F4F6] flex justify-center">
+          <div className=" w-[414px] h-[828px] mt-[216px] bg-[#FFFFFFBF] rounded-[48px]"></div>
+        </div>
+        <div className="w-[50%] bg-[#0F141E] flex justify-center">
+          <div className="w-[414px] h-[828px] mt-[216px] bg-[#111827BF] rounded-[48px]"></div>
+        </div>{" "}
+        <div className="border rounded-[50%] border-black h-[50px] w-[50px] absolute"></div>
+        <div className="border rounded-[50%] border-black h-[50px] w-[50px] absolute"></div>
+        <div className="border rounded-[50%] border-black h-[50px] w-[50px] absolute"></div>
+        <div className="border rounded-[50%] border-black h-[50px] w-[50px] absolute"></div>
+        <div className="border rounded-[50%] border-black h-[50px] w-[50px] absolute"></div>
+        <div className="border rounded-[50%] border-black h-[50px] w-[50px] absolute"></div>
+      </div>
     </div>
   );
 }
